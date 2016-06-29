876d4fcace199e24efa2f094f676611f0f9d53aa
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-06-29 21:41:53 +0300

    models and authentification

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
 class Question < ActiveRecord::Base
+
+  belongs_to :user
+  validates :user, presence: true
+  validates :text, presence: true
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,48 @@
+require 'openssl'
+
 class User < ActiveRecord::Base
+
+  #параметры работы модуля шифрования
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: create
+  validates_confirmation_of :password #эта строка говорит о том, что в этом объекте должно быть второе поле с именем password_confirmation
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    #если пароль у объекта для которого мы вычисляем зашифрованный пароль присутсвует, то:
+    if self.password.present?
+      #создаем т.н соль — рандомную строку, усложняющую задачу хакерам
+      self.password_salt= User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      #создаем hash пароля — длинная уникальная строка, из которой невозможно
+      #восстановить исходный пароль
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) #находим пользователя по email
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length,DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
