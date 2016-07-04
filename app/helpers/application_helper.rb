2f2d31e684b1da1c1d580bd2e0ef18c3d6485fa3
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-04 18:09:52 +0300

    Count of questions added + sklonenie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,39 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  class Sklonjator
+    def self.sklonenie (number, krokodil, krokodila, krokodilov)
+      if (number == nil || !number.is_a?(Numeric))
+        number = 0
+      end
+
+      ost = number%10
+      ost_100 = number%100
+
+      if (ost_100 == 11 || ost_100 == 12 || ost_100 == 13 || ost_100 == 14)
+        return krokodilov
+      end
+
+      if (ost == 1)
+        return krokodil
+      end
+
+      if (ost >= 2 && ost <=4)
+        return krokodila
+      end
+
+      if (ost > 4 || ost == 0)
+        return krokodilov
+      end
+    end
+  end
+
+
+  def questions_count(questions_array)
+    sklonenie = Sklonjator.sklonenie(questions_array.length.to_i,'вопрос', 'вопроса', 'вопросов')
+    return questions_array.length.to_s+" "+sklonenie
+  end
+
+
 end
