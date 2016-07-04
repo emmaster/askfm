efc38e8f824bef1e281be8e0c0eee0da538e522a
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-04 17:37:10 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+        User.new(
+                id: 1,
+                name: 'Vadim',
+                username: 'installero',
+                avatar_url: 'https://secure.gravatar.com/avatar/'
+        ),
+        User.new(
+                id: 2,
+                name: 'Misha',
+                username: 'aristofun'
+        )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello= "Привет, Мишаня!"
+    @user = User.new(
+      name: 'Alexey',
+      username: 'alexk',
+      avatar_url: 'https://secure.gravatar.com/avatar/6e65b6a033fba99124c8285c012af0bf',
+    )
+
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Какие вопросы', created_at: Date.parse('24.03.2016'))
+    ]
+
+    @new_question = Question.new
+
   end
 end
