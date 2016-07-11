490415292f609fa931529e74d9ab168d934912ff
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-11 15:50:09 +0300

    add color choice for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629165109) do
+ActiveRecord::Schema.define(version: 20160711123618) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160629165109) do
     t.string   "password_hash"
     t.string   "password_salt"
     t.string   "avatar_url"
+    t.string   "color"
   end
 
 end
