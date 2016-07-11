a6c3dac1e0b10a1c38f3b7ae80b245c9af198a1b
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-11 15:31:00 +0300

    automatic login after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
 
     redirect_to root_url, alert: 'вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
-
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно создан'
     else
       render 'new'
