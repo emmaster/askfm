830447e7e548259c06add6093d554845ed0a18bb
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-17 19:28:06 +0300

    session controller refactor according to webinar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.authenticate(params[:email],params[:password])
+    user = User.authenticate(params[:email],params[:password])
 
-    if @user.present?
-      session[:user_id] = @user.id
+    if user.present?
+      session[:user_id] = user.id
       redirect_to root_url, notice: 'вы успешно залогинились'
     else
       flash.now.alert = 'неправильный email или пароль'
@@ -17,5 +17,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: 'Вы разлогинились! Приходите еще'
+
   end
 end
